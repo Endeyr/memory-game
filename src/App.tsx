b29d8af229c4c3948110b715e0c59ad70e5b9fc4
Endeyr <aaron.thompson42594@gmail.com>
2024-03-27 10:11:44 -0400

    add base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 import './styles/App.css'
 
 function App() {
-
 	return (
 		<>
-			<p>Home</p>
+			<header className="header">
+				<h2 className="title">Memory Card Game</h2>
+				<p className="scoreboard">ScoreBoard: </p>
+			</header>
+			<section className="container">
+				<div className="row">
+					<div className="card"></div>
+					<div className="card"></div>
+					<div className="card"></div>
+					<div className="card"></div>
+					<div className="card"></div>
+					<div className="card"></div>
+				</div>
+				<div className="row">
+					<div className="card"></div>
+					<div className="card"></div>
+					<div className="card"></div>
+					<div className="card"></div>
+					<div className="card"></div>
+					<div className="card"></div>
+				</div>
+				<div className="row">
+					<div className="card"></div>
+					<div className="card"></div>
+					<div className="card"></div>
+					<div className="card"></div>
+					<div className="card"></div>
+					<div className="card"></div>
+				</div>
+			</section>
 		</>
 	)
 }
