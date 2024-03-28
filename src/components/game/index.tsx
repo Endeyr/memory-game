0bdea19b2c4e32c0e90c79ffe145601a9cfcff52
Endeyr <aaron.thompson42594@gmail.com>
2024-03-28 11:56:47 -0400

    basic logic for game

--- src/components/game/index.tsx
+++ src/components/game/index.tsx
@@ -1,7 +1,22 @@
+type CardType = {
+	id: number
+	image: string
+}
+
 const Game = ({ handleScore }: { handleScore: () => void }) => {
+	// init cards array
+	const cards: CardType[] = []
+	// fetch cards from api
+
+	// use effect for the fetch + callback function if fetch cancelled
+
+	// handle onClick, shuffles cards and adds to score or wrong answer
+
 	return (
 		<section className="container">
 			<div className="row">
+				{/* each card is in a grid */}
+				{cards && cards.map((card) => <div key={card.id}>{card.image}</div>)}
 				<div className="card">
 					<button onClick={handleScore}>click me</button>
 				</div>
